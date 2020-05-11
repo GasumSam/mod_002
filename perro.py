7ca957782d566bb6feb415300f65de6cbcdde347
GasumSam <gasumsam@gmail.com>
2020-05-11 20:09:56 +0200

    Herencia Perro Objetos Rantanplan

--- perro.py
+++ perro.py
@@ -9,22 +9,32 @@ class Perro():
         if self.peso >= 8:
             print("GUAU, GUAU")
         else:
-            print("guau, guau"
+            print("ay, ay")
             
             #El primer parámetro es siempre la instancia, recuerda
-           
+         
     def __str__(self):  #Método específico que devuelve una cadena
         return "Perro {}, e: {}, p: {}".format(self.nombre, self.edad, self.peso)
     
-                
+
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo 
+        self.trabajando = False  #Crea ubn atributo nuevo. Cuando se invoca por defecto a False
             
+    def __str__(self):   #Al ser una función que procede de la anterior clase, lo que se está haciendo es 'sobreescribir' el método
+        return "Perro de asistencia de {}".format(self.amo)
             
-            
-            
-            
-            
-            
-            
+    def pasear(self):
+        print("{} ayudo a mi dueño, {} a pasear". format(self.nombre, self.amo))
+
+    def ladrar(self):   #Sobreescribimos ladrar
+        if self.trabajando:
+            print("shhhhh, no puedo ladrar")
+        else:
+            Perro.ladrar(self)  #Invoco a la instancia original  (Método padre)
+    
             
             
             
